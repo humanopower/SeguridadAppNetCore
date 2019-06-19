d99dbbe2e8000ca8a28ff9bceb1d428d5665dae0
yamil <yamil.israel.ortega@pemex.com>
2019-06-19 17:40:09 -0500

    Se agregan metodos restantes a security Service

--- SeguridadApp.NetCoreSolution/SeguridadAppAPI/Controllers/SecurityServiceController.cs
+++ SeguridadApp.NetCoreSolution/SeguridadAppAPI/Controllers/SecurityServiceController.cs
@@ -1,53 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using EntityLibrary;
-using Microsoft.AspNetCore.Http;
+﻿using EntityLibrary;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using SecurityLogicLibrary;
+using System;
+using System.Collections.Generic;
 
 namespace SeguridadAppAPI.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class SecurityServiceController : ControllerBase
-    {
+	[Route("api/[controller]")]
+	[ApiController]
+	public class SecurityServiceController : ControllerBase
+	{
 		private readonly IConfiguration _config;
+
 		public SecurityServiceController(IConfiguration configuration)
 		{
 			_config = configuration;
 		}
 
-		// POST api/values
-		[HttpPost]
-		public ActionResult<Response> Post([FromBody] string value)
-		{
-			var resp = new Response();
-			resp.Message = "HEllo";
-			resp.Result =true;
-			return resp;
-		}
 
 		// POST api/values
 		[HttpGet]
 		public IActionResult Get()
 		{
 			var resp = new Response();
-			resp.Message = "HEllo";
+			resp.Message = "Welcome to the SecurityService. This is test basic Get method. Refer to manual for allowed operations.";
 			resp.Result = true;
-			
+			resp.Result = true;
+
 			return Ok(resp);
 		}
 
-		
-		// POST api/values
+		/// <summary>
+		/// Returns a true or false response against Active Directory and Application Security Service
+		/// </summary>
+		// POST api/Authenticate
 		[HttpPost("Authenticate")]
-	   public IActionResult Authenticate([FromBody] JObject data)
+		public IActionResult Authenticate([FromBody] JObject data)
 		{
-
 			var resp = new Response();
 			var userAuthenticated = new User();
 			try
@@ -56,17 +48,180 @@ namespace SeguridadAppAPI.Controllers
 				var userId = data["userId"].ToString();
 				var password = data["password"].ToString();
 				var applicationName = data["applicationName"].ToString();
-				var securityServiceOperation = new SecurityServiceOperations(_config);				
+				var securityServiceOperation = new SecurityServiceOperations(_config);
 				resp = securityServiceOperation.Authenticate(domain, userId, password, applicationName, out userAuthenticated);
 				var returnObject = new { Response = resp, User = userAuthenticated };
 				return Ok(returnObject);
 			}
 			catch (Exception ex)
 			{
-				return BadRequest("Algo salió mal");
+				return BadRequest("La solicitud no fué enviada correctamente");
 			}
-			
-			
+		}
+
+		/// <summary>
+		/// Returns a true or false response against Active Directory only.
+		/// </summary>
+		// POST api/AuthenticateADOnly
+		[HttpPost("AuthenticateADOnly")]
+		public IActionResult AuthenticateADOnly([FromBody] JObject data)
+		{
+			var resp = new Response();
+			var userAuthenticated = new User();
+			try
+			{
+				var domain = data["domain"].ToString();
+				var userId = data["userId"].ToString();
+				var password = data["password"].ToString();
+				var securityServiceOperation = new SecurityServiceOperations(_config);
+				resp = securityServiceOperation.AuthenticateADOnly(domain, userId, password, out userAuthenticated);
+				var returnObject = new { Response = resp, User = userAuthenticated };
+				return Ok(returnObject);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest("La solicitud no fué enviada correctamente");
+			}
+		}
+
+		/// <summary>
+		/// Returns a true or false response against Security schema
+		/// </summary>
+		// POST api/Authorize
+		[HttpPost("Authorize")]
+		public IActionResult Authorize([FromBody] JObject data)
+		{
+			var resp = new Response();
+			var userAuthenticated = new User();
+			try
+			{
+				userAuthenticated = JsonConvert.DeserializeObject<User>(data["user"].ToString());
+				var applicationName = data["applicationName"].ToString();
+				var applicationPassword = data["applicationPassword"].ToString();
+				var operation = data["operation"].ToString();
+				using (var securityServiceOperation = new SecurityServiceOperations(_config))
+				{
+					resp = securityServiceOperation.Authorize(userAuthenticated, applicationName, applicationPassword, operation);
+				}
+				var returnObject = new { Response = resp };
+				return Ok(returnObject);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest("La solicitud no fué enviada correctamente");
+			}
+		}
+
+		/// <summary>
+		/// Get user information and roles assigned to an user.
+		/// </summary>
+		// POST api/GetUserInformationAndRoles
+		[HttpPost("GetUserInformationAndRoles")]
+		public IActionResult GetUserInformationAndRoles([FromBody] JObject data)
+		{
+			var resp = new Response();
+			var user = new User();
+			var roleList = new List<Role>();
+			try
+			{
+				var applicationName = data["applicationName"].ToString();
+				var applicationPassword = data["applicationPassword"].ToString();
+				var userId = data["userId"].ToString();
+				using (var securityServiceOperation = new SecurityServiceOperations(_config))
+				{
+					resp = securityServiceOperation.GetUserInformationAndRoles(applicationName, applicationPassword, userId, out user, out roleList);
+				}
+				var returnObject = new { Response = resp, User = user, roleUserList = roleList };
+				return Ok(returnObject);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest("La solicitud no fué enviada correctamente");
+			}
+		}
+
+		/// <summary>
+		/// Get user information and Operations assigned to an user.
+		/// </summary>
+		// POST api/GetUserInformationAndOperations
+		[HttpPost("GetUserInformationAndOperations")]
+		public IActionResult GetUserInformationAndOperations([FromBody] JObject data)
+		{
+			var resp = new Response();
+			var operationList = new List<Operation>();
+			var userAuthenticated = new User();
+			try
+			{
+				userAuthenticated = JsonConvert.DeserializeObject<User>(data["user"].ToString());
+				var applicationName = data["applicationName"].ToString();
+				var applicationPassword = data["applicationPassword"].ToString();
+				var userId = data["userId"].ToString();
+				using (var securityServiceOperation = new SecurityServiceOperations(_config))
+				{
+					resp = securityServiceOperation.GetUserInformationAndOperations(userAuthenticated, applicationName, applicationPassword, out operationList);
+				}
+				var returnObject = new { Response = resp, operationUserList = operationList };
+				return Ok(returnObject);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest("La solicitud no fué enviada correctamente");
+			}
+		}
+
+		/// <summary>
+		/// Get user list by role
+		/// </summary>
+		// POST api/GetUserListByRole
+		[HttpPost("GetUserListByRole")]
+		public IActionResult GetUserListByRole([FromBody] JObject data)
+		{
+			var resp = new Response();
+			var operationList = new List<Operation>();
+			var userListFindedByRole = new List<User>();
+			try
+			{
+				var applicationName = data["applicationName"].ToString();
+				var applicationPassword = data["applicationPassword"].ToString();
+				var roleName = data["roleName"].ToString();
+				using (var securityServiceOperation = new SecurityServiceOperations(_config))
+				{
+					resp = securityServiceOperation.GetUserListByRole(applicationName, applicationPassword, roleName, out userListFindedByRole);
+				}
+				var returnObject = new { Response = resp, userList = userListFindedByRole };
+				return Ok(returnObject);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest("La solicitud no fué enviada correctamente");
+			}
+		}
+
+		/// <summary>
+		/// Get user list by Application
+		/// </summary>
+		// POST api/GetUserListByRole
+		[HttpPost("GetUserListByApplication")]
+		public IActionResult GetUserListByApplication([FromBody] JObject data)
+		{
+			var resp = new Response();
+			var operationList = new List<Operation>();
+			var userListFindedByApplication = new List<User>();
+			try
+			{
+				var applicationName = data["applicationName"].ToString();
+				var applicationPassword = data["applicationPassword"].ToString();
+				using (var securityServiceOperation = new SecurityServiceOperations(_config))
+				{
+					resp = securityServiceOperation.GetUserListByApplication(applicationName, applicationPassword, out userListFindedByApplication);
+				}
+				var returnObject = new { Response = resp, userList = userListFindedByApplication };
+				return Ok(returnObject);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest("La solicitud no fué enviada correctamente");
+			}
 		}
 	}
 }
